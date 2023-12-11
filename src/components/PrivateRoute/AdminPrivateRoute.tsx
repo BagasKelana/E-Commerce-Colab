86f3a0098feed3b111d536454b7c48ab1d1765e5
BagasKelana <hilalbagas@gmail.com>
2023-12-12 01:16:21 +0700

    feat: auth-system, global-state [SignIn, SignUp]

--- src/components/PrivateRoute/AdminPrivateRoute.tsx
+++ src/components/PrivateRoute/AdminPrivateRoute.tsx
@@ -1,8 +1,9 @@
+import { RootState } from '@/redux/store';
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const AdminPrivateRoute = () => {
-    const { currentUser } = useSelector((state) => state.user);
+    const { currentUser } = useSelector((state: RootState) => state.user);
     return currentUser ? <Outlet /> : <Navigate to={'/login'} />;
 };
 export default AdminPrivateRoute;
