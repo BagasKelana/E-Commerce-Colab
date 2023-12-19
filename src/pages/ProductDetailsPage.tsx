10f28c312bc06790265cbe4cee6c296804acb36c
BrassyThermal <muhammadyusrildevj@gmail.com>
2023-12-19 20:35:44 +0700

    feat: add product detail pages

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,5 +1,26 @@
+import { useLocation } from 'react-router-dom';
+
+import Layout from '@/layout/Layout';
+import ProductDetails from '@/components/product-details-page/ProductDetails';
+import PopulerCategories from '@/components/landing-page/PopulerCategories';
+import ProductSection from '@/components/landing-page/ProductSection';
+import Footer from '@/components/Footer';
+
 const ProductDetailsPage = () => {
-    return <div className="w-full text-4xl">halo</div>;
+    const { state } = useLocation();
+
+    if (!state || !state.productData) {
+        <h1>Error!</h1>;
+    }
+
+    return (
+        <Layout>
+            <ProductDetails />
+            <PopulerCategories />
+            <ProductSection />
+            <Footer />
+        </Layout>
+    );
 };
 
 export default ProductDetailsPage;
