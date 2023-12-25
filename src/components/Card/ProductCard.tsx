a11e869fbc8292ecfbea8a6d37e7ca9002061842
BagasKelana <hilalbagas@gmail.com>
2023-12-25 10:35:17 +0700

    fix: responsive product-card

--- src/components/Card/ProductCard.tsx
+++ src/components/Card/ProductCard.tsx
@@ -1,6 +1,5 @@
-import { cn } from '@/lib/utils';
 import { useNavigate } from 'react-router-dom';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 
 type CardProps = React.ComponentProps<typeof Card>;
 
@@ -8,10 +7,9 @@ type ProductCardProps = CardProps & {
     name: string;
     src: string;
     price: number;
-    className?: string;
 };
 
-const ProductCard = ({ className, name, src, price }: ProductCardProps) => {
+const ProductCard = ({ name, src, price }: ProductCardProps) => {
     const navigate = useNavigate();
     const handleProductClick = () => {
         const productData = {
@@ -22,36 +20,32 @@ const ProductCard = ({ className, name, src, price }: ProductCardProps) => {
         navigate(`/product/${name}`, { state: { productData } });
     };
     return (
-        <div className={cn('inline-block w-full  ', className)}>
-            <div className=" relative min-w-full">
-                <div className="h-full" onClick={handleProductClick}>
-                    <Card className=" min-w-[100px] h-[257.844px]  rounded-md">
-                        <img
-                            className="object-cover w-full"
-                            height={512}
-                            width={512}
-                            src={`${
-                                import.meta.env.VITE_DEVELOPE_API_IMG
-                            }/${src}`}
-                            alt="card-img"
-                            loading="lazy"
-                        />
+        <div className="h-full" onClick={handleProductClick}>
+            <Card className="w-full h-full md:min-w-[100px] md:min-h-[257.844px] rounded-lg overflow-hidden">
+                <CardHeader className="p-0">
+                    <img
+                        className="object-cover w-full"
+                        height={512}
+                        width={512}
+                        src={`${import.meta.env.VITE_DEVELOPE_API_IMG}/${src}`}
+                        alt="card-img"
+                        loading="lazy"
+                    />
+                </CardHeader>
 
-                        <hr />
-                        <CardContent className="p-2">
-                            <p className="font-medium text-xs line-claps-with-ellipsis">
-                                {name}
-                            </p>
-                            <h3 className="text-base font-bold break-words">
-                                Rp
-                                {price?.toLocaleString('id-ID', {
-                                    currency: 'IDR'
-                                })}
-                            </h3>
-                        </CardContent>
-                    </Card>
-                </div>
-            </div>
+                <hr />
+                <CardContent className="py-4 px-2 md:p-2">
+                    <p className="font-medium text-xs line-claps-with-ellipsis">
+                        {name}
+                    </p>
+                    <h3 className="text-base font-bold break-words">
+                        Rp
+                        {price?.toLocaleString('id-ID', {
+                            currency: 'IDR'
+                        })}
+                    </h3>
+                </CardContent>
+            </Card>
         </div>
     );
 };
