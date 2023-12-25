08fc61fdc5b8d0a8960bd2c3d362cf3aef2a77e0
BagasKelana <hilalbagas@gmail.com>
2023-12-26 06:24:43 +0700

    fix: pagination

--- src/components/product-page/Pagination.tsx
+++ src/components/product-page/Pagination.tsx
@@ -1,105 +1,71 @@
-import { buttonVariants } from '@/components/ui/button';
-import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import { useSearchParams } from 'react-router-dom';
 import { Link as LinkProps } from '@/hook/useFetch';
-
 
 interface PaginationProps {
     links?: LinkProps[];
 }
 
-const Pagination: React.FC<PaginationProps> = ({ url, links, querySearch }) => {
+const Pagination: React.FC<PaginationProps> = ({ links }) => {
+    const [searchParams, setSearchParams] = useSearchParams();
 
-    // Buat objek URLSearchParams dari query string
-    let searchParams = new URLSearchParams(querySearch);
-
-    // Hapus parameter 'page'
-    searchParams.delete('page');
-
-    // Ambil query string yang sudah diubah
-    let updatedQuerySearch = searchParams.toString();
-
-    function getAllUrlParams(url) {
-
-        // get query string from url (optional) or window
-        var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-      
-        // we'll store the parameters here
-        var obj = {};
-      
-        // if query string exists
-        if (queryString) {
-      
-          // stuff after # is not part of query string, so get rid of it
-          queryString = queryString.split('#')[0];
-      
-          // split our query string into its component parts
-          var arr = queryString.split('&');
-      
-          for (var i = 0; i < arr.length; i++) {
-            // separate the keys and the values
-            var a = arr[i].split('=');
-      
-            // set parameter name and value (use 'true' if empty)
-            var paramName = a[0];
-            var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-      
-            // (optional) keep case consistent
-            paramName = paramName.toLowerCase();
-            if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-      
-            // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-            if (paramName.match(/\[(\d+)?\]$/)) {
-      
-              // create key if it doesn't exist
-              var key = paramName.replace(/\[(\d+)?\]/, '');
-              if (!obj[key]) obj[key] = [];
-      
-              // if it's an indexed array e.g. colors[2]
-              if (paramName.match(/\[\d+\]$/)) {
-                // get the index value and add the entry at the appropriate position
-                var index = /\[(\d+)\]/.exec(paramName)[1];
-                obj[key][index] = paramValue;
-              } else {
-                // otherwise add the value to the end of the array
-                obj[key].push(paramValue);
-              }
-            } else {
-              // we're dealing with a string
-              if (!obj[paramName]) {
-                // if it doesn't exist, create property
-                obj[paramName] = paramValue;
-              } else if (obj[paramName] && typeof obj[paramName] === 'string'){
-                // if property does exist and it's a string, convert it to an array
-                obj[paramName] = [obj[paramName]];
-                obj[paramName].push(paramValue);
-              } else {
-                // otherwise add the property
-                obj[paramName].push(paramValue);
-              }
-            }
-          }
+    const handleOnClick = (
+        page: string,
+        firstPage: string,
+        lastPage: string
+    ) => {
+        const currentPage = searchParams.get('page') || 1;
+        console.log(firstPage, lastPage, currentPage);
+        if (page && !isNaN(+page) && +page > 0) {
+            searchParams.set('page', page);
+            setSearchParams(searchParams);
+            return;
         }
-      
-        return obj;
-      }
+        if (page) {
+            if (
+                page === '&laquo; Previous' &&
+                currentPage &&
+                currentPage > firstPage
+            ) {
+                searchParams.set('page', String(+currentPage - 1));
+                setSearchParams(searchParams);
+                return;
+            }
+            if (
+                page === 'Next &raquo;' &&
+                currentPage &&
+                currentPage < lastPage
+            ) {
+                searchParams.set('page', String(+currentPage + 1));
+                setSearchParams(searchParams);
+                return;
+            }
+        }
+    };
 
     return (
         <>
-            <div>
-                {links?.map((link, index) => (
-                   <Link
-                        key={index}
-                        className={link.active ? buttonVariants({ variant: 'ghost' }) : buttonVariants({ variant: 'outline' })}
-                        to={`${url}?${updatedQuerySearch}&page=${link.url ? getAllUrlParams(link.url).page : ''}`}
+            {links?.map((link, index) => {
+                return (
+                    <Button
+                        onClick={() => {
+                            handleOnClick(
+                                link.label,
+                                links[1].label,
+                                links[links.length - 2].label
+                            );
+                        }}
+                        key={index * 10000}
+                        variant={link.active ? 'primery' : 'default'}
                     >
-                        {link.label === "&laquo; Previous"
-                            ? "«"
-                            : link.label === "Next &raquo;"
-                            ? "»"
+                        {link.label === '&laquo; Previous'
+                            ? '«'
+                            : link.label === 'Next &raquo;'
+                            ? '»'
                             : link.label}
-                    </Link>
-                ))}
-            </div>
+                    </Button>
+                );
+            })}
         </>
     );
 };
