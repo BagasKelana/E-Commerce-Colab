6b0acb22035b937e10787931da05423b15053a78
BagasKelana <hilalbagas@gmail.com>
2023-12-25 10:36:19 +0700

    style: layout margin for better ui

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,6 @@
+import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
-import useNavScroll from '@/hook/useNavScroll';
+
 import { cn } from '@/lib/utils';
 
 type LayoutProps = {
@@ -8,21 +9,18 @@ type LayoutProps = {
 };
 
 const Layout = ({ children, bgColor }: LayoutProps) => {
-    const { isSticky } = useNavScroll();
     return (
         <div className="w-full h-full ">
-            <Navbar
-                stikyNavBar={isSticky}
-                className=" shadow-sm shadow-gray-200 "
-            />
+            <Navbar />
             <div
                 className={cn(
-                    'h-full w-full transition-all ease-in-out duration-300 mt-[123px]',
+                    'h-full w-full transition-all ease-in-out duration-300 mt-[80px] md:mt-[123px]',
                     bgColor
                 )}
             >
-                <main>{children}</main>
+                <main className="w-full h-full">{children}</main>
             </div>
+            <Footer />
         </div>
     );
 };
