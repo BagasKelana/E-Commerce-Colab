50927833488c4d874285a76b526313e11c26ce9c
BagasKelana <hilalbagas@gmail.com>
2023-12-26 12:22:28 +0700

    feat: add disable variant button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-    'inline-flex items-center  justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+    'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
     {
         variants: {
             variant: {
@@ -19,7 +19,8 @@ const buttonVariants = cva(
                     'bg-secondary text-secondary-foreground hover:bg-secondary/80',
                 ghost: 'hover:bg-accent hover:text-accent-foreground',
                 link: 'text-primary underline-offset-4 hover:underline',
-                primery: 'bg-amber-500 text-white hover:bg-amber-500/90'
+                primery: 'bg-amber-500 text-white hover:bg-amber-500/90',
+                disable: 'bg-gray-500/80 text-white pointer-events-none'
             },
             size: {
                 default: 'h-10 px-4 py-2',
