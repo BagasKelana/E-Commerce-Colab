548771614f46b4c583f553f1ecfa9e3b5fbbffe7
BagasKelana <hilalbagas@gmail.com>
2023-12-26 11:51:30 +0700

    fix: product not found

--- src/components/product-page/ProductSection.tsx
+++ src/components/product-page/ProductSection.tsx
@@ -12,9 +12,11 @@ type ProductSectionProps = {
 
 const ProductSection: React.FC<ProductSectionProps> = ({ children, url }) => {
     const { data, error, loading } = useFetch<FetchAllProduct>(
-        `${url && `${import.meta.env.VITE_DEVELOPE_API}/product?${url}`}`,
+        `${import.meta.env.VITE_DEVELOPE_API}/product?${url}`,
         null
     );
+
+    console.log(data)
 
     const renderSkeleton = useCallback(() => {
         return Array.from({ length: 10 }, (_, index) => (
@@ -22,25 +24,37 @@ const ProductSection: React.FC<ProductSectionProps> = ({ children, url }) => {
         ));
     }, []);
 
+    const productRendering = () => {
+        if (data?.data?.data) {
+            if (data?.data?.data?.length > 0) {
+                return data?.data?.data?.map((product) => (
+                    <ProductCard
+                        slug={product.slug}
+                        key={product.slug}
+                        name={product.name}
+                        src={product.product_image?.[0]?.image}
+                        price={product.price}
+                    />
+                ));
+            }
+            return (
+                <div className="col-span-5 row-span-2 place-content-center justify-items-center">
+                    Product Tidak Ditemukan
+                </div>
+            );
+        }
+    };
+
     return (
         <ErrorHandling error={error}>
             <div className="flex justify-between xl:justify-end items-center mb-4">
                 {children}
             </div>
             <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-x-4 gap-y-5 place-items-stretch mb-4 w-full">
-                {loading
-                    ? renderSkeleton()
-                    : data?.data?.data?.map((product) => (
-                          <ProductCard
-                              key={product.slug}
-                              name={product.name}
-                              src={product.product_image?.[0]?.image}
-                              price={product.price}
-                          />
-                      ))}
+                {loading ? renderSkeleton() : productRendering()}
             </section>
             <div className="w-full flex justify-center py-4">
-                <Pagination links={data?.data?.links} />
+                <Pagination url={'/product'} links={data?.data?.links} />
             </div>
         </ErrorHandling>
     );
