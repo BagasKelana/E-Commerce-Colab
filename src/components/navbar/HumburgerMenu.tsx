ef04426e5a4f64b83bd8c1ea4b8de41851aeeb4e
BagasKelana <hilalbagas@gmail.com>
2024-01-03 06:09:10 +0700

    fix : humburgerMenu

--- src/components/navbar/HumburgerMenu.tsx
+++ src/components/navbar/HumburgerMenu.tsx
@@ -1,4 +1,4 @@
-import { Menu } from 'lucide-react';
+import { Heart, List, Menu, MenuSquare, Star, StoreIcon } from 'lucide-react';
 import {
     Sheet,
     SheetClose,
@@ -9,65 +9,82 @@ import {
     SheetTrigger
 } from '@/components/ui/sheet';
 import { Button } from '@/components/ui/button';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { Link } from 'react-router-dom';
 
 export default function HumburgerMenu() {
+    const { currentUser } = useSelector((state: RootState) => state.user);
     return (
         <Sheet>
             <SheetTrigger asChild>
                 <li className="ml-2 lg:ml-4 relative inline-block md:hidden">
-                    <Menu className="w-10 h-10 p-2 text-slate-100" />
+                    <Menu className="w-10 h-10 p-2 text-slate-100 cursor-pointer" />
                 </li>
             </SheetTrigger>
             <SheetContent className="overflow-auto no-scrollbar">
                 <SheetHeader>
                     <SheetTitle>Menu Utama</SheetTitle>
                 </SheetHeader>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Esse nulla maxime adipisci, corporis ea error ducimus quos
-                    maiores fuga? Nobis ipsam nemo, earum odio officiis quasi
-                    animi aut pariatur consectetur!
+                <div className="w-full flex justify-between gap-4 my-4">
+                    <Link
+                        className="text-white py-1 px-2 text-center bg-teal-700 w-full"
+                        aria-label="Masuk"
+                        to={'/signin'}
+                    >
+                        Masuk
+                    </Link>
+                    <Link
+                        className="text-teal-700 py-1 border border-teal-700 text-center font-semibold px-2 bg-slate-50 w-full"
+                        aria-label="Daftar"
+                        to={'/signup'}
+                    >
+                        Daftar
+                    </Link>
                 </div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Esse nulla maxime adipisci, corporis ea error ducimus quos
-                    maiores fuga? Nobis ipsam nemo, earum odio officiis quasi
-                    animi aut pariatur consectetur!
+                <hr />
+                <div className="w-full my-4">
+                    <ul className="space-y-5">
+                        <li className="whitespace-normal flex gap-2 items-center ">
+                            <MenuSquare className=" text-black " />
+                            Daftar Transaksi
+                        </li>
+                        <li className="whitespace-normal flex gap-2">
+                            <Star className="text-black" />
+                            Ulasan
+                        </li>
+                        <li className="whitespace-normal flex gap-2">
+                            <Heart />
+                            Wishlist
+                        </li>
+                        <li className="whitespace-normal flex gap-2">
+                            <StoreIcon />
+                            Toko yang difollow
+                        </li>
+                    </ul>
                 </div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Esse nulla maxime adipisci, corporis ea error ducimus quos
-                    maiores fuga? Nobis ipsam nemo, earum odio officiis quasi
-                    animi aut pariatur consectetur!
-                </div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Esse nulla maxime adipisci, corporis ea error ducimus quos
-                    maiores fuga? Nobis ipsam nemo, earum odio officiis quasi
-                    animi aut pariatur consectetur!
-                </div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Esse nulla maxime adipisci, corporis ea error ducimus quos
-                    maiores fuga? Nobis ipsam nemo, earum odio officiis quasi
-                    animi aut pariatur consectetur!
-                </div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Esse nulla maxime adipisci, corporis ea error ducimus quos
-                    maiores fuga? Nobis ipsam nemo, earum odio officiis quasi
-                    animi aut pariatur consectetur!
-                </div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Esse nulla maxime adipisci, corporis ea error ducimus quos
-                    maiores fuga? Nobis ipsam nemo, earum odio officiis quasi
-                    animi aut pariatur consectetur!
-                </div>
+                <hr />
                 <SheetFooter>
-                    <SheetClose asChild>
-                        <Button>Save changes</Button>
-                    </SheetClose>
+                    <div className="w-full my-4">
+                        <ul className="space-y-5">
+                            <li className="whitespace-normal flex gap-2 items-center ">
+                                <MenuSquare className=" text-black " />
+                                Daftar Transaksi
+                            </li>
+                            <li className="whitespace-normal flex gap-2">
+                                <Star className="text-black" />
+                                Ulasan
+                            </li>
+                            <li className="whitespace-normal flex gap-2">
+                                <Heart />
+                                Wishlist
+                            </li>
+                            <li className="whitespace-normal flex gap-2">
+                                <StoreIcon />
+                                Toko yang difollow
+                            </li>
+                        </ul>
+                    </div>
                 </SheetFooter>
             </SheetContent>
         </Sheet>
