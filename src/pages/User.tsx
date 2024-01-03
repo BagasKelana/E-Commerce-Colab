53e1621c2509607535e275cff5c4e39ed1826e2e
BagasKelana <hilalbagas@gmail.com>
2024-01-04 06:07:07 +0700

    feat: side-bar user-profile

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,33 +1,80 @@
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import UserProfileForm from '@/components/user-page/UserProfileForm';
-import { FetchCurrentUserType } from '@/fetch';
-import useFetch from '@/hook/useFetch';
+
 import Layout from '@/layout/Layout';
-import { cn } from '@/lib/utils';
 
-import { Link, useOutletContext } from 'react-router-dom';
+import { RootState } from '@/redux/store';
+import { MenuSquare, Pencil, User2 } from 'lucide-react';
+import { useSelector } from 'react-redux';
 
-type CurrentUser = {
-    token: string;
-};
+import { NavLink } from 'react-router-dom';
 
 const User = () => {
-    const token = useOutletContext<CurrentUser>();
-
-    const { data, loading, error, reFetchData } =
-        useFetch<FetchCurrentUserType>(
-            'https://roughy-loyal-daily.ngrok-free.app/api/user',
-            null,
-            `Bearer ${token}`
-        );
-
-    console.log(data);
-
+    const { currentUser } = useSelector((state: RootState) => state.user);
     return (
         <Layout>
-            <div className="w-full pl-4 md:px-20 py-5 ">
-                <div className="w-full grid grid-cols-4 gap-4">
-                    <DocsSidebarNavItems />
-                    <main className="w-full col-span-3 bg-white p-4">
+            <div className="w-full min-h-screen h-full pl-4 md:px-20 py-5 bg-slate-100">
+                <div className="w-full grid grid-cols-5 gap-4">
+                    <div className="w-full col-span-1 flex flex-col ">
+                        <div className="w-full my-4 flex gap-4">
+                            <Avatar className="h-12 w-12">
+                                <AvatarImage
+                                    src={
+                                        currentUser?.image
+                                            ? `${
+                                                  import.meta.env
+                                                      .VITE_DEVELOPE_API_IMG
+                                              }/${currentUser?.image}`
+                                            : 'images/profile_3135715.png'
+                                    }
+                                />
+                            </Avatar>
+                            <div className="flex flex-col my-1">
+                                <h3 className="text-sm font-semibold">
+                                    {currentUser?.name}
+                                </h3>
+                                <p className="text-muted-foreground text-sm whitespace-nowrap flex items-center gap-1">
+                                    <Pencil className="h-4 w-4" />
+                                    <span>Ubah Profile</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="mt-7">
+                            <ul className="font-medium text-sm flex flex-col gap-4 select-none">
+                                <NavLink
+                                    to="/user/profile"
+                                    className={({ isActive, isPending }) =>
+                                        isPending
+                                            ? 'pending'
+                                            : isActive
+                                            ? 'text-teal-800'
+                                            : 'text-black'
+                                    }
+                                >
+                                    <li className="flex whitespace-nowrap gap-2">
+                                        <User2 className="h-5 w-5" />
+                                        <span> My Profile</span>
+                                    </li>
+                                </NavLink>
+                                <NavLink
+                                    to="/user/my-order"
+                                    className={({ isActive, isPending }) =>
+                                        isPending
+                                            ? 'pending'
+                                            : isActive
+                                            ? 'text-teal-800'
+                                            : 'text-black'
+                                    }
+                                >
+                                    <li className="flex whitespace-nowrap gap-2">
+                                        <MenuSquare className="h-5 w-5" />
+                                        <span>My Order</span>
+                                    </li>
+                                </NavLink>
+                            </ul>
+                        </div>
+                    </div>
+                    <main className="w-full col-span-4 bg-white p-4">
                         <div>
                             <h2 className="font-medium text-base">
                                 My Profile
@@ -40,12 +87,7 @@ const User = () => {
 
                         <hr className="mt-4" />
                         <div>
-                            <UserProfileForm
-                                currentUser={data?.data?.user}
-                                isLoading={loading}
-                                isError={error}
-                                reFetch={reFetchData}
-                            />
+                            <UserProfileForm />
                         </div>
                     </main>
                 </div>
@@ -53,51 +95,5 @@ const User = () => {
         </Layout>
     );
 };
-
-export function DocsSidebarNavItems() {
-    const items = [
-        {
-            title: 'My Profile',
-            disabled: false,
-            external: true
-        },
-        {
-            title: 'My Profile',
-            disabled: false,
-            external: true
-        },
-        {
-            title: 'My Profile',
-            disabled: false,
-            external: true
-        }
-    ];
-
-    return items?.length ? (
-        <div className="grid grid-flow-row auto-rows-max text-sm">
-            {items.map((item, index) =>
-                !item.disabled ? (
-                    <Link
-                        key={index}
-                        to="/"
-                        className={cn(
-                            'flex w-full items-center rounded-md p-2 hover:underline',
-
-                            'bg-muted'
-                        )}
-                        target={item.external ? '_blank' : ''}
-                        rel={item.external ? 'noreferrer' : ''}
-                    >
-                        {item.title}
-                    </Link>
-                ) : (
-                    <span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">
-                        {item.title}
-                    </span>
-                )
-            )}
-        </div>
-    ) : null;
-}
 
 export default User;
