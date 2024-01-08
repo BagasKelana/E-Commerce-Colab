cb4f474cd521501415a7c87d78b3bcaac66aa839
BagasKelana <hilalbagas@gmail.com>
2024-01-08 19:32:00 +0700

    feat: user-order ui

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -3,13 +3,13 @@ import UserProfileForm from '@/components/user-page/UserProfileForm';
 
 const User = () => {
     return (
-        <main className="w-full col-span-5 bg-white p-4">
+        <div className="w-full">
             <UserProfileDescription title="My Profile">
                 Kelola informasi profil Anda untuk mengontrol, melindungi dan
                 mengamankan akun
             </UserProfileDescription>
             <UserProfileForm />
-        </main>
+        </div>
     );
 };
 
