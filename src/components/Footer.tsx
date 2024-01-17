b753e9cc1d24a864df23a2cb04302c24419aba23
Vincent Alfarieco <vincent111232@gmail.com>
2024-01-17 21:11:09 +0700

    feat(ui): Add footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,121 +7,109 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
     return (
         <footer className="bg-gray-900">
-            <div className="container flex flex-col flex-wrap px-4 py-16 mx-auto md:items-center lg:items-start md:flex-row md:flex-nowrap">
-                <div className="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
+            <div className="container flex flex-col flex-wrap px-4 py-4 mx-auto md:items-center lg:items-start md:flex-row md:flex-nowrap">
+                <div className="flex-shrink-0 pt-3 md:pt-5 w-64 mx-auto text-center md:mx-0 md:text-left">
                     <Link to={'/'} className="text-2xl text-white">
-                        Entahlah
+                        Email
                     </Link>
-                    <p className="mt-2 text-xs text-justify text-gray-400">
-                        Di buat dengan kedelai pilihan, marika
+                    <p className="pt-5 md:pt-2 text-xs text-justify text-gray-400">
+                        Bantuan dan Saran
                     </p>
-                    <div className="flex mt-4">
+                    <div className="flex pt-4 flex-col gap-4">
                         <Input type="email" placeholder="Email" />
                         <Button variant="destructive">Subscribe</Button>
                     </div>
-                    <div className="flex justify-center mt-4 space-x-4 lg:mt-2">
+                    <div className="flex justify-center pt-6 md:pt-3 gap-8 lg:mt-2">
                         <Link to={''}>
-                            <Facebook className="text-blue-500" />
+                            <Facebook className='text-white' />
                         </Link>
                         <Link to={''}>
-                            <Twitter className="text-sky-300" />
+                            <Twitter className="text-white" />
                         </Link>
                         <Link to={''}>
-                            <Instagram className="text-pink-500" />
+                            <Instagram className="text-white" />
                         </Link>
                         <Link to={''}>
-                            <Linkedin className="text-blue-400" />
+                            <Linkedin className="text-white" />
                         </Link>
                     </div>
                 </div>
-                <div className="justify-between w-full mt-4 text-center lg:flex">
+                <div className="justify-between pl-14 md:text-left w-full mt-4 lg:flex">
                     <div className="w-full px-4 lg:w-1/3 md:w-1/2">
                         <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-                            Quick Links
+                            Customer Service
                         </h2>
                         <ul className="mb-8 space-y-2 text-sm list-none">
                             <li>
                                 <Link to={'/'} className="text-gray-300">
-                                    Link 1
+                                    Help Desk
                                 </Link>
                             </li>
                             <li>
                                 <Link to={'/'} className="text-gray-300">
-                                    Link 2
+                                    Support 24/7
                                 </Link>
                             </li>
                             <li>
                                 <Link to={'/'} className="text-gray-300">
-                                    Link 3
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={'/'} className="text-gray-300">
-                                    Link 4
+                                    Shopee Community
                                 </Link>
                             </li>
                         </ul>
                     </div>
-                    <div className="w-full px-4 lg:w-1/3 md:w-1/2">
+                    <div className="w-full px-4 text-left lg:w-1/3 md:w-1/2">
                         <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-                            Quick Links
+                            Tentang Tokopedia
                         </h2>
                         <ul className="mb-8 space-y-2 text-sm list-none">
                             <li>
                                 <Link to={'/'} className="text-gray-300">
-                                    Link 1
+                                    Karir
                                 </Link>
                             </li>
                             <li>
                                 <Link to={'/'} className="text-gray-300">
-                                    Link 2
+                                    Blog
                                 </Link>
                             </li>
                             <li>
                                 <Link to={'/'} className="text-gray-300">
-                                    Link 3
+                                    Hak Kekayaan Intelektual
                                 </Link>
                             </li>
                             <li>
                                 <Link to={'/'} className="text-gray-300">
-                                    Link 4
+                                    Mitra
                                 </Link>
                             </li>
                         </ul>
                     </div>
-                    <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-                        <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-                            Quick Links
+                    <div className="w-full text-left px-4 lg:w-1/3 md:w-1/2">
+                        <h2 className="mb-2 font-bold tracking-widest  text-gray-100">
+                            Bantuan dan Panduan
                         </h2>
                         <ul className="mb-8 space-y-2 text-sm list-none">
                             <li>
                                 <Link to={'/'} className="text-gray-300">
-                                    Link 1
+                                    Shopee care
                                 </Link>
                             </li>
                             <li>
                                 <Link to={'/'} className="text-gray-300">
-                                    Link 2
+                                    Kebijakan Privasi
                                 </Link>
                             </li>
                             <li>
                                 <Link to={'/'} className="text-gray-300">
-                                    Link 3
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={'/'} className="text-gray-300">
-                                    Link 4
+                                    Syarat dan Ketentuan
                                 </Link>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div className="flex justify-center -mt-12">
-                <p className="text-center text-white pb-2">
-                    @2024 Titik & Lana.
-                </p>
+            <div className="flex justify-center pb-10 pt-2">
+                <p className="text-center text-white">@2024 Titik & Lana.</p>
             </div>
         </footer>
     );
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import { cn } from '@/lib/utils';
 
@@ -13,6 +14,7 @@ const Layout = ({ children, className }: LayoutProps) => {
             <div className="h-full w-full transition-all ease-in-out duration-300 mt-[80px] md:mt-[110px]">
                 <main className="w-full h-full">{children}</main>
             </div>
+            <Footer />
         </div>
     );
 };
