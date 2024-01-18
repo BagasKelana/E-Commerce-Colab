d0df7b92f0fe1ee1b94400aa80dd819cbe31e2be
BagasKelana <hilalbagas@gmail.com>
2024-01-18 15:17:17 +0700

    ...

---
+++ src/components/product-page/filter/FilterList.tsx
@@ -0,0 +1,23 @@
+import { FilterListProps } from '../product-type';
+
+const FilterList: React.FC<FilterListProps> = ({
+    onClick,
+    id,
+    children,
+    className,
+    isLoading
+}) => {
+    return (
+        <li
+            onClick={onClick}
+            id={id}
+            className={`${className || 'text-gray-600'} ${
+                isLoading && 'pointer-events-none'
+            } cursor-pointer p-2 select-none hover:bg-gray-100 rounded `}
+        >
+            {children}
+        </li>
+    );
+};
+
+export default FilterList;
