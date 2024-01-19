b6fd28704cb938515a64011b981e00f1a7c015ee
BagasKelana <hilalbagas@gmail.com>
2024-01-19 18:56:02 +0700

    feat: landing page hero for mobile device

--- src/components/landing-page/LandingHero.tsx
+++ src/components/landing-page/LandingHero.tsx
@@ -9,13 +9,14 @@ import { cn } from '@/lib/utils';
 import { NextArrow, PrevArrow } from '../Carousel/Arrow';
 
 const img = [
-    '/images/429e3395-e54d-4ff2-8d54-47784086daa0.jpg.webp',
-    '/images/09ca38b8-fa15-4e7c-a852-da2be08b5042.jpg.webp',
-    '/images/id-50009109-96e46f7e61898a3baddbae5f7d65f1e5_xxhdpi.png',
+    '/images//discount/discount1.jpg',
+    '/images//discount/discount2.jpg',
+    '/images//discount/discount3.jpg'
+];
+
+const banner = [
     '/images/id-50009109-b36a2f24939caa00a796df79fd0e0c99_xxhdpi.png',
-    '/images/6561a9a3627d81eb4c09c1ee.jpg',
-    '/images/6561a9a3627d81eb4c09c1ee.jpg',
-    '/images/6561a9a3627d81eb4c09c1ee.jpg'
+    '/images/id-50009109-96e46f7e61898a3baddbae5f7d65f1e5_xxhdpi.png'
 ];
 
 type DotSlide = {
@@ -23,7 +24,7 @@ type DotSlide = {
     nextSlide: number;
 };
 
-const LandingHero = () => {
+export const LandingHero = () => {
     const [dotSlide, setDotSlide] = useState<DotSlide>({
         currentSlide: 0,
         nextSlide: 1
@@ -51,13 +52,13 @@ const LandingHero = () => {
                     padding: '0px'
                 }}
             >
-                <ul className="dots-list flex justify-center p-4"> {dots} </ul>
+                <ul className="dots-list flex justify-center p-2 "> {dots} </ul>
             </div>
         ),
         customPaging: (index) => (
             <div
                 className={cn(
-                    'h-1 w-1 md:h-2 md:w-2 bg-gray-400/50 hover:bg-gray-300 rounded-full transition duration-200 ease-in-out ',
+                    'h-1 w-1 md:h-2 md:w-2 bg-slate-200/50 hover:bg-slate-200 rounded-full transition duration-200 ease-in-out ',
                     index === dotSlide.nextSlide && 'bg-white hover:bg-white'
                 )}
             />
@@ -77,21 +78,86 @@ const LandingHero = () => {
         ]
     };
     return (
-        <div className="w-full pt-4 px-4 md:px-6 lg:px-20 my-4 container">
-            <div className="group h-full relative w-full">
-                <Slider {...settings}>
-                    {img.map((img) => (
-                        <img
-                            key={img}
-                            className="object-cover object-center aspect-[12/5] md:aspect-[12/4]  xl:aspect-[12/3]  rounded overflow-hidden"
-                            src={img}
-                            alt="img-slider"
-                        />
-                    ))}
-                </Slider>
-            </div>
+        <div className="group h-full relative w-full hidden md:block">
+            <Slider {...settings}>
+                {banner.map((img) => (
+                    <img
+                        key={img}
+                        className="object-cover object-center h-full w-full overflow-hidden"
+                        src={img}
+                        alt="img-slider"
+                    />
+                ))}
+            </Slider>
         </div>
     );
 };
 
-export default LandingHero;
+export const LandingHeroMobile = () => {
+    const [dotSlide, setDotSlide] = useState<DotSlide>({
+        currentSlide: 0,
+        nextSlide: 1
+    });
+
+    const settings: Settings = {
+        beforeChange: (currentSlide, nextSlide) => {
+            setDotSlide((current) => ({ ...current, currentSlide, nextSlide }));
+        },
+        draggable: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 3000,
+        cssEase: 'ease-in-out',
+        useCSS: true,
+        appendDots: (dots) => (
+            <div
+                style={{
+                    bottom: '0px',
+                    padding: '0px'
+                }}
+            >
+                <ul className="dots-list flex justify-center p-2 "> {dots} </ul>
+            </div>
+        ),
+        customPaging: (index) => (
+            <div
+                className={cn(
+                    'h-1 w-1 md:h-2 md:w-2 bg-slate-200/50 hover:bg-slate-200 rounded-full transition duration-200 ease-in-out ',
+                    index === dotSlide.nextSlide && 'bg-white hover:bg-white'
+                )}
+            />
+        ),
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    draggable: true
+                }
+            }
+        ]
+    };
+    return (
+        <div className="group h-full relative w-full row-span-3 col-span-4 md:hidden">
+            <Slider {...settings}>
+                {img.map((img) => (
+                    <img
+                        key={img}
+                        className="object-cover object-center h-full w-full overflow-hidden"
+                        src={img}
+                        alt="img-slider"
+                    />
+                ))}
+            </Slider>
+        </div>
+    );
+};
