25da0df95eff058746803927e6bff538c2427a27
BagasKelana <hilalbagas@gmail.com>
2024-01-23 19:23:47 +0700

    refactor: naming data

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -12,6 +12,7 @@ const ProductDetailsPage = () => {
 
     const url = `${import.meta.env.VITE_USER_PRODUCT_DETAIL}/${slug}`;
     const { data } = useFetch<DetailProductProps>(url, null);
+    const product = data?.data;
     const [imageIndex, setImageIndex] = useState(0);
 
     const handleOnMouseEnter = (index: number) => {
@@ -23,60 +24,54 @@ const ProductDetailsPage = () => {
                 <div className="flex gap-4">
                     <div className="w-2/5">
                         <div className="flex overflow-hidden h-[450px] relative">
-                            {data?.data?.product_image?.map(
-                                (product, index) => {
-                                    return (
-                                        index === imageIndex && (
-                                            <div
-                                                key={product.id}
-                                                className="flex w-full items-center justify-center rounded overflow-hidden"
-                                            >
-                                                <img
-                                                    src={showImageAPI(
-                                                        product.image
-                                                    )}
-                                                    alt="images-product"
-                                                    className="object-cover h-full"
-                                                />
-                                            </div>
-                                        )
-                                    );
-                                }
-                            )}
-                        </div>
-                        <div className="grid grid-cols-5 w-full gap-2 mt-2">
-                            {data?.data?.product_image?.map(
-                                (product, index) => {
-                                    return (
+                            {product?.product_image?.map((product, index) => {
+                                return (
+                                    index === imageIndex && (
                                         <div
                                             key={product.id}
-                                            className={cn(
-                                                'rounded aspect-[6/5] flex justify-center border-2',
-                                                index === imageIndex &&
-                                                    'border-teal-600'
-                                            )}
+                                            className="flex w-full items-center justify-center rounded overflow-hidden"
                                         >
                                             <img
-                                                onMouseEnter={() => {
-                                                    handleOnMouseEnter(index);
-                                                }}
                                                 src={showImageAPI(
                                                     product.image
                                                 )}
                                                 alt="images-product"
-                                                className="cursor-pointer h-full"
+                                                className="object-cover h-full"
                                             />
                                         </div>
-                                    );
-                                }
-                            )}
+                                    )
+                                );
+                            })}
+                        </div>
+                        <div className="grid grid-cols-5 w-full gap-2 mt-2">
+                            {product?.product_image?.map((product, index) => {
+                                return (
+                                    <div
+                                        key={product.id}
+                                        className={cn(
+                                            'rounded aspect-[6/5] flex justify-center border-2',
+                                            index === imageIndex &&
+                                                'border-teal-600'
+                                        )}
+                                    >
+                                        <img
+                                            onMouseEnter={() => {
+                                                handleOnMouseEnter(index);
+                                            }}
+                                            src={showImageAPI(product.image)}
+                                            alt="images-product"
+                                            className="cursor-pointer h-full"
+                                        />
+                                    </div>
+                                );
+                            })}
                         </div>
                     </div>
                     <div className="w-3/5 flex flex-col">
                         <h2 className="text-xl font-semibold">
-                            {data?.data.name}
+                            {product?.name}
                         </h2>
-                        <h1>Rp {formatRupiah(data?.data.price)}</h1>
+                        <h1>Rp {formatRupiah(product?.price)}</h1>
                     </div>
                 </div>
             </div>
