dd0831ea49c5c79c11c7b0c6fb1d2713d7677d39
BagasKelana <hilalbagas@gmail.com>
2024-01-24 12:30:21 +0700

    style: new ui for Table Product

--- src/components/dashboard-page/Products/table/DataTable.tsx
+++ src/components/dashboard-page/Products/table/DataTable.tsx
@@ -16,13 +16,15 @@ import {
 } from '@/components/ui/table';
 
 import DataTablePagination from '../../table-component/DataTabelPagination';
+import DataLoader from '@/components/DataLoader';
+import FilterProductTable from './FilterProductTable';
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
     data: TData[];
     isLoading: boolean;
-    totalProduct: number | null;
-    nextPagination: string | null;
+    totalProduct?: number | null;
+    nextPagination?: string | null;
 }
 
 export default function DataTable<TData, TValue>({
@@ -48,27 +50,16 @@ export default function DataTable<TData, TValue>({
     const renderTableHeader = () => {
         return table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header, index) => {
-                    return index === 1 ? (
-                        <TableHead className="w-[5%]" key={header.id}>
-                            {header.isPlaceholder
-                                ? null
-                                : flexRender(
-                                      header.column.columnDef.header,
-                                      header.getContext()
-                                  )}
-                        </TableHead>
-                    ) : (
-                        <TableHead className="py-6" key={header.id}>
-                            {header.isPlaceholder
-                                ? null
-                                : flexRender(
-                                      header.column.columnDef.header,
-                                      header.getContext()
-                                  )}
-                        </TableHead>
-                    );
-                })}
+                {headerGroup.headers.map((header) => (
+                    <TableHead className="py-5" key={header.id}>
+                        {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                  header.column.columnDef.header,
+                                  header.getContext()
+                              )}
+                    </TableHead>
+                ))}
             </TableRow>
         ));
     };
@@ -76,48 +67,62 @@ export default function DataTable<TData, TValue>({
     return (
         <>
             <div className="w-full">
-                <div className="shadow-md shadow-slate-400/80 rounded-xl overflow-hidden">
-                    <div className="border-none bg-white px-4 pt-2">
-                        <Table>
-                            <TableHeader>{renderTableHeader()}</TableHeader>
+                <div className="shadow-border shadow-slate-400/30 rounded-xl overflow-hidden">
+                    <div className="border-none bg-white  pt-2 ">
+                        <FilterProductTable isLoading={isLoading} />
+                        <DataLoader spinner isLoading={isLoading}>
+                            <Table>
+                                <TableHeader>{renderTableHeader()}</TableHeader>
 
-                            <TableBody>
-                                {table.getRowModel().rows?.length ? (
-                                    table.getRowModel().rows.map((row) => (
-                                        <TableRow
-                                            key={row.id}
-                                            data-state={
-                                                row.getIsSelected() &&
-                                                'selected'
-                                            }
-                                        >
-                                            {row
-                                                .getVisibleCells()
-                                                .map((cell) => (
-                                                    <TableCell key={cell.id}>
-                                                        {flexRender(
-                                                            cell.column
-                                                                .columnDef.cell,
-                                                            cell.getContext()
-                                                        )}
-                                                    </TableCell>
-                                                ))}
+                                <TableBody>
+                                    {table.getRowModel().rows?.length ? (
+                                        table.getRowModel().rows.map((row) => {
+                                            console.log(row.id);
+                                            return (
+                                                <TableRow
+                                                    key={row.id}
+                                                    data-state={
+                                                        row.getIsSelected() &&
+                                                        'selected'
+                                                    }
+                                                >
+                                                    {row
+                                                        .getVisibleCells()
+                                                        .map((cell) => {
+                                                            return (
+                                                                <TableCell
+                                                                    key={
+                                                                        cell.id
+                                                                    }
+                                                                >
+                                                                    {flexRender(
+                                                                        cell
+                                                                            .column
+                                                                            .columnDef
+                                                                            .cell,
+                                                                        cell.getContext()
+                                                                    )}
+                                                                </TableCell>
+                                                            );
+                                                        })}
+                                                </TableRow>
+                                            );
+                                        })
+                                    ) : (
+                                        <TableRow>
+                                            <TableCell
+                                                colSpan={columns.length}
+                                                className="h-24 text-center"
+                                            >
+                                                No results.
+                                            </TableCell>
                                         </TableRow>
-                                    ))
-                                ) : (
-                                    <TableRow>
-                                        <TableCell
-                                            colSpan={columns.length}
-                                            className="h-24 text-center"
-                                        >
-                                            No results.
-                                        </TableCell>
-                                    </TableRow>
-                                )}
-                            </TableBody>
-                        </Table>
+                                    )}
+                                </TableBody>
+                            </Table>
+                        </DataLoader>
                     </div>
-                    <div className="flex items-center justify-end space-x-2 py-4 bg-white px-8 pb-2">
+                    <div className="flex items-center justify-end space-x-2 pt-2 pb-6 bg-white px-8 ">
                         <DataTablePagination
                             nextPagination={nextPagination}
                             table={table}
