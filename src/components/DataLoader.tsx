c6b9cfd1704c38108b2f097f1054337289051ad1
BagasKelana <hilalbagas@gmail.com>
2024-01-24 12:29:04 +0700

    feat: add spinner loader in DalaLoader Component

--- src/components/DataLoader.tsx
+++ src/components/DataLoader.tsx
@@ -1,16 +1,30 @@
 import SkeletonCard from './Card/SkeletonCard';
+import SpinnerLoading from './ui/spinner';
 
 type DataLoaderProps = {
     isLoading: boolean;
     children: React.ReactNode;
     dataCount?: number;
+    spinner?: boolean;
+    className?: string;
 };
 
 const DataLoader: React.FC<DataLoaderProps> = ({
     isLoading,
     children,
-    dataCount = 10
+    dataCount = 10,
+    spinner,
+    className
 }) => {
+    if (spinner) {
+        return isLoading ? (
+            <div className="h-[200px] flex items-center justify-center">
+                <SpinnerLoading className={className} />
+            </div>
+        ) : (
+            <>{children}</>
+        );
+    }
     const renderSkeleton = () => {
         return Array.from({ length: dataCount }, (_, index) => (
             <SkeletonCard key={index} />
