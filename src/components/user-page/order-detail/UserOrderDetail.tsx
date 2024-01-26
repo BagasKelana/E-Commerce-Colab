a84c7b7427247661e5cdf91c1ff41be1fe51bce9
BagasKelana <hilalbagas@gmail.com>
2024-01-26 22:06:18 +0700

    style: bg, shadow

--- src/components/user-page/order-detail/UserOrderDetail.tsx
+++ src/components/user-page/order-detail/UserOrderDetail.tsx
@@ -117,94 +117,100 @@ const UserOrderDetail = () => {
 
 const OrderCartDetail = () => {
     return (
-        <div className="mb-4 p-2 bg-slate-200">
-            <div className="w-full flex flex-col mb-2 p-4 rounded bg-white">
-                <div className="w-full flex justify-between mb-3">
-                    <h2 className="text-lg">
-                        {firstLetterToUpper(fakeOrder.data.order.status)}
-                    </h2>
-                    <span className="text-teal-700 font-medium text-sm self-center">
-                        Lihat Detail
-                    </span>
+        <div className="mb-4 px-2 w-full ">
+            <div className="shadow-border shadow-slate-300 bg-white py-4 rounded-md">
+                <div className="w-full flex flex-col mb-2 p-4 ">
+                    <div className="w-full flex justify-between mb-3">
+                        <h2 className="text-lg">
+                            {firstLetterToUpper(fakeOrder.data.order.status)}
+                        </h2>
+                        <span className="text-teal-700 font-medium text-sm self-center">
+                            Lihat Detail
+                        </span>
+                    </div>
+                    <div className="w-full flex justify-between">
+                        <p className="text-sm">No. Invoice</p>
+                        <span className="text-teal-700 font-medium">
+                            {fakeOrder.data.order.invoice}
+                        </span>
+                    </div>
+                    <div className="w-full flex justify-between">
+                        <p className="text-sm">Tanggal Pembelian</p>
+                        <span className="text-sm">
+                            {fakeOrder.data.order.date}
+                        </span>
+                    </div>
                 </div>
-                <div className="w-full flex justify-between">
-                    <p className="text-sm">No. Invoice</p>
-                    <span className="text-teal-700 font-medium">
-                        {fakeOrder.data.order.invoice}
-                    </span>
-                </div>
-                <div className="w-full flex justify-between">
-                    <p className="text-sm">Tanggal Pembelian</p>
-                    <span className="text-sm">{fakeOrder.data.order.date}</span>
-                </div>
-            </div>
-            <div className="p-4 bg-white">
-                <div className="flex justify-between mb-2">
-                    <h2 className="text-lg">Detail Product</h2>
-                </div>
-                {fakeOrder?.data.products.data.map((orderProduct) => {
-                    return (
-                        <section className="flex w-full justify-between p-6 gap-5 border border-slate-300 rounded bg-white">
-                            <div className="w-full flex  gap-2">
-                                <div className="flex gap-2 justify-center p-2">
-                                    <div className="w-[50px] h-[50px]">
-                                        <img
-                                            className="object-cover aspect-square"
-                                            src={
-                                                '/images/product/1ee9fd68-309c-473f-b5f9-b28c13c59d7b.png'
-                                            }
-                                            alt="product-order"
-                                        />
+                <div className="p-4 ">
+                    <div className="flex justify-between mb-2">
+                        <h2 className="text-lg">Detail Product</h2>
+                    </div>
+                    {fakeOrder?.data.products.data.map((orderProduct) => {
+                        return (
+                            <section className="flex w-full justify-between p-6 gap-5 border border-slate-300 rounded ">
+                                <div className="w-full flex  gap-2">
+                                    <div className="flex gap-2 justify-center p-2">
+                                        <div className="w-[50px] h-[50px]">
+                                            <img
+                                                className="object-cover aspect-square"
+                                                src={
+                                                    '/images/product/1ee9fd68-309c-473f-b5f9-b28c13c59d7b.png'
+                                                }
+                                                alt="product-order"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="w-full py-2">
+                                        <h2 className="line-claps-with-ellipsis font-semibold text-base">
+                                            {orderProduct.product.name}
+                                        </h2>
+                                        <p className="text-sm text-slate-600">
+                                            {orderProduct?.qty} x{' '}
+                                            {formatRupiah(
+                                                orderProduct?.product?.price
+                                            )}
+                                        </p>
                                     </div>
                                 </div>
-                                <div className="w-full py-2">
-                                    <h2 className="line-claps-with-ellipsis font-semibold text-base">
-                                        {orderProduct.product.name}
-                                    </h2>
-                                    <p className="text-sm text-slate-600">
-                                        {orderProduct?.qty} x{' '}
-                                        {formatRupiah(
-                                            orderProduct?.product?.price
-                                        )}
-                                    </p>
+                                <div className="w-full flex flex-col items-center gap-2 text-end pt-2">
+                                    <div className="w-full">
+                                        <p>Total Harga</p>
+                                        <h2 className="text-base font-semibold">
+                                            Rp
+                                            {formatRupiah(
+                                                orderProduct.subtotal
+                                            )}
+                                        </h2>
+                                        <Button
+                                            size="sm"
+                                            variant="outlinePrimery"
+                                            className="w-32 mt-2"
+                                        >
+                                            Beli Lagi
+                                        </Button>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="w-full flex flex-col items-center gap-2 text-end pt-2">
-                                <div className="w-full">
-                                    <p>Total Harga</p>
-                                    <h2 className="text-base font-semibold">
-                                        Rp
-                                        {formatRupiah(orderProduct.subtotal)}
-                                    </h2>
-                                    <Button
-                                        size="sm"
-                                        variant="outlinePrimery"
-                                        className="w-32 mt-2"
-                                    >
-                                        Beli Lagi
-                                    </Button>
-                                </div>
-                            </div>
-                        </section>
-                    );
-                })}
-            </div>
-            <div className="w-full flex flex-col mt-2 p-4 rounded bg-white">
-                <div className="w-full flex justify-between mb-3">
-                    <h2 className="text-lg">Info Pengiriman</h2>
+                            </section>
+                        );
+                    })}
                 </div>
-                <div className="flex flex-col gap-2 text-sm">
-                    <div className="w-full flex justify-between">
-                        <p>Nama</p>
-                        <span>{fakeOrder.data.order.name}</span>
+                <div className="w-full flex flex-col mt-2 p-4 rounded ">
+                    <div className="w-full flex justify-between mb-3">
+                        <h2 className="text-lg">Info Pengiriman</h2>
                     </div>
-                    <div className="w-full flex justify-between">
-                        <p>Lokasi Pengiriman</p>
-                        <span>{fakeOrder.data.order.address}</span>
-                    </div>
-                    <div className="w-full flex justify-between">
-                        <p>Nomer Hp</p>
-                        <span>{fakeOrder.data.order.phone}</span>
+                    <div className="flex flex-col gap-2 text-sm">
+                        <div className="w-full flex justify-between">
+                            <p>Nama</p>
+                            <span>{fakeOrder.data.order.name}</span>
+                        </div>
+                        <div className="w-full flex justify-between">
+                            <p>Lokasi Pengiriman</p>
+                            <span>{fakeOrder.data.order.address}</span>
+                        </div>
+                        <div className="w-full flex justify-between">
+                            <p>Nomer Hp</p>
+                            <span>{fakeOrder.data.order.phone}</span>
+                        </div>
                     </div>
                 </div>
             </div>
